94dd9ef213f4a901a368a3e830f900f468319ad5
John Bolt <johnwillbolt@gmail.com>
2025-02-24 18:41:58 -0500

    Set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './party/templates/party/**/*.html',
+        './party/static/party/js/**/*.js',
+        './party/static/party/src/**/*.css',
+        './**/*.py',
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Roboto"] // setting the default font-family
+        },
+        extend: {
+            fontFamily: {
+                "roboto": ["Roboto", "sans-serif"], // making possible to use classes font-roboto and font-dancing-script
+                "dancing-script": ["Dancing Script", "cursive"],
+            }
+        },
+    },
+    plugins: [],
+}
